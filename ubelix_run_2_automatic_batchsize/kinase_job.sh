4d8350920508bb2962d5b83f9b42388668632d5c
gutheb <your.email@example.com>
2026-04-06 15:15:40 +0200

    automatic batch size chosen

---
+++ ubelix_run_2_automatic_batchsize/kinase_job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#------------------------
+#SBATCH --account=gratis
+#------------------------
+#SBATCH --job-name="tdatasc_kinase"
+#SBATCH --time=01:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=7
+#SBATCH --mem-per-cpu=1G
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:rtx4090:2
+
+# Your code below this line
+module load CUDA/12.2.0
+module load Anaconda3
+eval "$(conda shell.bash hook)"
+conda activate kinaseenv
+
+jupyter nbconvert --to notebook --execute \
+    --ExecutePreprocessor.kernel_name=kinaseenv \
+    /storage/homefs/hg21b070/datasc_kinase/kinaseforubelix.ipynb --output kinaseforubelix_out.ipynb
